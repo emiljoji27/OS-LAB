fa7d744f81f0fe8eef59927bdb7bd798a7498486
Emil Joji <67428445+emiljoji27@users.noreply.github.com>
2021-09-23 11:44:33 +0530

    Add files via upload

---
+++ producer.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+#include<sys/ipc.h>
+#include<sys/shm.h>
+#include<sys/types.h>
+#include<string.h>
+#include<errno.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+
+struct mem
+{
+  int buffer[10];
+  int mutex;
+};
+ 
+void wait(struct mem* ptr)
+{
+ if(ptr->mutex==0)
+ {
+   printf("Please wait.Consumer is consuming...\n");
+   while(1)
+   {
+     if(ptr->mutex==1)
+       break;
+   }
+ }
+ ptr->mutex--;
+}
+
+void signal(struct mem* ptr)
+{
+ ptr->mutex++;
+}
+
+void main()
+{
+      struct mem node;
+      struct mem* ptr = &node;
+      int x;
+      key_t key = ftok("shmfile",65);  // ftok to generate unique key
+      int shmid = shmget(key,1024,0666|IPC_CREAT);  // shmget returns an identifier to shmid
+      ptr = (struct mem*) shmat(shmid,(void*)0,0);  // shmat to attach to shared memory
+      ptr->mutex=1;
+      for(int i=0;i<10;i++)
+      {
+        x = rand()%1000;
+        ptr->buffer[i]=x;
+        wait(ptr);
+        printf("Please wait.Producer is producing %d...\n",ptr->buffer[i]);
+        sleep(1.5);
+        printf("Producer produced : %d\n",ptr->buffer[i]);
+        signal(ptr);
+        sleep(2.5);
+      }
+      shmdt(ptr);  //detach from shared memory
+}
+
---
+++ sharedmem-writer.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<sys/ipc.h>
+#include<sys/shm.h>
+#include<sys/types.h>
+#include<string.h>
+#include<errno.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+
+
+void main()
+{
+      key_t key = ftok("shmfile",65);  // ftok to generate unique key
+      int shmid = shmget(key,1024,0666|IPC_CREAT);  // shmget returns an identifier to shmid
+      char *str = (char*) shmat(shmid,(void*)0,0);  // shmat to attach to shared memory
+      printf("Write Data : ");
+      fgets(str,20,stdin);
+      printf("Data written in memory: %s\n",str);
+      shmdt(str);  //detach from shared memory
+}
+
